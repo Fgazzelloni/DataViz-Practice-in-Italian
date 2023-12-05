cd6cfa82d68fc8816dabe198a1667084a3e1cab4
fgazzelloni <docksbox@pm.me>
2023-12-05 16:10:43 +0000

    updating helper with directions for setting own credentials on posit cloud

--- helper00.R
+++ helper00.R
@@ -2,5 +2,17 @@
 
 pkgs <- c("tidyverse","ggthemes","sysfonts","showtext",
           "cowplot","ggtext")
-
 install.packages(pkgs)
+
+## Connetti il tuo GitHub repository:
+## in TERMINAL
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+## in CONSOLE - se hai già un TOKEN
+install.packages("gitcreds")
+gitcreds::gitcreds_set()
+## in CONSOLE - se NON hai un TOKEN
+install.packages("usethis") 
+usethis::create_github_token() # apre una pagina GitHub nel browser per creare un TOKEN
+install.packages("gitcreds")
+gitcreds::gitcreds_set() # inserisci il TOKEN
